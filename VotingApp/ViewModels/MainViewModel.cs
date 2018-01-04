bfd7767fc0b2cb3b73e1b38bb4c99285c5d6ba08
Jerzy Jaskiewicz <jurek.jaskiewicz@gmail.com>
2018-01-04 22:49:27 +0100

    added menu to MainView

--- VotingApp/ViewModels/MainViewModel.cs
+++ VotingApp/ViewModels/MainViewModel.cs
@@ -7,14 +7,53 @@ using System.Threading.Tasks;
 
 namespace EventVoting.VotingApp.ViewModels
 {
-    public class MainViewModel: Screen
+    public class MainViewModel : Screen
     {
+        private EventViewModel _selectedEvent;
+
         public MainViewModel()
         {
             var context = ApplicationData.Current.DbContext;
             context.Appliance.Add(new Appliance { DeviceId = Guid.NewGuid().ToByteArray() });
             context.SaveChanges();
             context.Dispose();
+        }
+
+        public void EventCreate()
+        {
+            SelectedEvent = new EventViewModel();
+        }
+
+        public void EventSelect()
+        {
+
+        }
+
+        public void EventClose()
+        {
+            SelectedEvent = null;
+        }
+
+        public bool CanEventCreate => SelectedEvent == null;
+
+        public bool CanEventSelect => SelectedEvent == null;
+
+        public bool CanEventClose => SelectedEvent != null;
+
+
+        public EventViewModel SelectedEvent
+        {
+            get => _selectedEvent;
+            set
+            {
+                if (_selectedEvent == value)
+                    return;
+                _selectedEvent = value;
+                NotifyOfPropertyChange();
+                NotifyOfPropertyChange(nameof(CanEventClose));
+                NotifyOfPropertyChange(nameof(CanEventCreate));
+                NotifyOfPropertyChange(nameof(CanEventSelect));
+            }
         }
 
     }
