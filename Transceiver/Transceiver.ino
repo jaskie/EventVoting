a69519c2545c8c70080233b3eb619ea5ea641ddc
Jerzy Jaskiewicz <jurek.jaskiewicz@gmail.com>
2018-02-08 22:49:30 +0100

    added base-64 library to communicate with host

--- Transceiver/Transceiver.ino
+++ Transceiver/Transceiver.ino
@@ -1,9 +1,14 @@
 #include <LoRaVoting.h>
 #include <Arduino.h>
+#include <rBase64.h>
+
+#define BUFFER_SIZE 256
+byte serial_buffer[BUFFER_SIZE];
+byte message_buffer[BUFFER_SIZE];
 
 void setup() {
   // put your setup code here, to run once:
-	Serial.begin(9600);                   // initialize serial
+	Serial.begin(115200);                   // initialize serial
 	LoRaVoting.init();
 	LoRaVoting.ReceivedResponseCallback(onReceiveResponse);
 	if (LoRaVoting.IsReady())
@@ -13,6 +18,12 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 	LoRaVoting.SendMessage(BroadcastMessage(RegisteredDevices, "asking for registered devices"));
+	if (Serial.available())
+	{
+		size_t bytes_readed = Serial.readBytesUntil('\0', serial_buffer, BUFFER_SIZE);
+		size_t decoded_length = rbase64_decode((char*)serial_buffer, (char*)message_buffer, bytes_readed);
+
+	}
 	//LoRaVoting.SendMessage(BroadcastMessage(NewDevice, "ABCDEFGHIJKLMNOP"));
 	delay(5000);
 }
